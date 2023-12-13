bc5917fe586c7959bd13c91633b947c6715973f5
Ampflower <ampflower@outlook.com>
2023-12-12 22:15:52 -0600

    Build shaded jars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@
 plugins {
 	// Apply the application plugin to add support for building a CLI application in Java.
 	application
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 repositories {
